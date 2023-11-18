97de5f9fc4018c37b307d3c3f756b7831167c1c9
harshil1277 <kaneria.1@iitj.ac.in>
2023-11-18 16:09:52 +0530

    Updated Pharmacist Dashboard for Update Medicine

--- src/PharmacistDashboard.java
+++ src/PharmacistDashboard.java
@@ -17,6 +17,7 @@ public class PharmacistDashboard extends javax.swing.JFrame {
      */
     public PharmacistDashboard() {
         initComponents();
+        setLocationRelativeTo(null);
     }
 
     public PharmacistDashboard(String tempUsername) {
@@ -166,6 +167,7 @@ public class PharmacistDashboard extends javax.swing.JFrame {
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
+        new UpdateMedicine().setVisible(true);
 
     }//GEN-LAST:event_jButton5ActionPerformed
 
