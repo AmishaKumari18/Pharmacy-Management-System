9ebe26dab93877011ce6e93087d9c1b0d175deb3
harshil1277 <kaneria.1@iitj.ac.in>
2023-11-26 03:07:19 +0530

    Updated Pharmacist Dashboard for Sell medicine

--- src/PharmacistDashboard.java
+++ src/PharmacistDashboard.java
@@ -147,7 +147,7 @@ public class PharmacistDashboard extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-
+        new SellMedicine(username).setVisible(true);    
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
