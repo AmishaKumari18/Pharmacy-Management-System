925a1d117187a3f09b08c9f3d9bb0c2e5c099a56
harshil1277 <kaneria.1@iitj.ac.in>
2023-11-26 03:08:59 +0530

    Added Table for bill

--- src/dao/Tables.java
+++ src/dao/Tables.java
@@ -15,9 +15,10 @@ public class Tables {
         try{
             Connection con = ConnectionProvider.getCon();
             Statement st = con.createStatement();
-            st.executeUpdate("create table if not exist appuser(appuser_pk int AUTO_INCREMENT primary key, userRole varchar(256), name varchar(256), dob varchar(50), mobileNumber varchar(50), email varchar(256), username varchar(256), password varchar(50), address varchar(256) )");
-            st.executeUpdate("insert into appuser(userRole,name,dob,mobileNumber,email,username,password,address) values('Admin','Admin','15-03-2003','7276464726','kaneria.1@iitj.ac.in','admin','admin','Nanded,India' )");
-            st.executeUpdate("CREATE TABLE IF NOT EXISTS medicine (medicine_pk INT AUTO_INCREMENT, pharmacist_username VARCHAR(256), uniqueId VARCHAR(256),name VARCHAR(256), companyName VARCHAR(256),quantity BIGINT,price BIGINT, PRIMARY KEY (medicine_pk, pharmacist_username));");
+//            st.executeUpdate("create table if not exist appuser(appuser_pk int AUTO_INCREMENT primary key, userRole varchar(256), name varchar(256), dob varchar(50), mobileNumber varchar(50), email varchar(256), username varchar(256), password varchar(50), address varchar(256) )");
+//            st.executeUpdate("insert into appuser(userRole,name,dob,mobileNumber,email,username,password,address) values('Admin','Admin','15-03-2003','7276464726','kaneria.1@iitj.ac.in','admin','admin','Nanded,India' )");
+//            st.executeUpdate("CREATE TABLE IF NOT EXISTS medicine (medicine_pk INT AUTO_INCREMENT, pharmacist_username VARCHAR(256), uniqueId VARCHAR(256),name VARCHAR(256), companyName VARCHAR(256),quantity BIGINT,price BIGINT, PRIMARY KEY (medicine_pk, pharmacist_username));");
+            st.executeUpdate("create table bill(bill_pk int AUTO_INCREMENT primary key, billId varchar(256), billDate varchar(256), totalPaid bigint, generatedBy varchar(256))");    
             JOptionPane.showMessageDialog(null,"Table Created Successfully");
         }
         catch(Exception e){
