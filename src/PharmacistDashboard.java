0e6a7cb5de9e36c2be8c3bd574bd87a53c02a886
harshil1277 <kaneria.1@iitj.ac.in>
2023-12-06 03:29:42 +0530

    Updated for view medicine

--- src/PharmacistDashboard.java
+++ src/PharmacistDashboard.java
@@ -182,6 +182,7 @@ public class PharmacistDashboard extends javax.swing.JFrame {
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
         // TODO add your handling code here:
+        new ViewBill(username).setVisible(true);
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
